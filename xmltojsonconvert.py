079eb9afeafadcc17b18bf738d278c9c34a5dfb2
tpareek18 <68942780+tpareek18@users.noreply.github.com>
2023-12-31 11:04:07 +0400

    Update xmltojsonconvert.py
    cleaned up code

--- xmltojsonconvert.py
+++ xmltojsonconvert.py
@@ -3,9 +3,6 @@ import json
 import os
 from pathlib import Path
 
-# json_file = '/Users/tanay/Desktop/ResearchWork/2006/2006cat_xml.json'
-# xml_file = '/Users/tanay/Desktop/ResearchWork/2006/2006cat_xml.xml'
-
 def output_patent(xml_string):
     patent = xmltodict.parse(xml_string)
     with open(json_file, 'a') as f:
@@ -21,26 +18,11 @@ def read_file(file):
                 xml_string = ''
             xml_string += line
 
-
-# f = open(json_file, 'w+')
-# f.write('[\n')
-# f.close()
-
-# read_file(xml_file)
-
-# f = open(json_file, 'a')
-# f.write('{\n')
-# f.write('}\n')
-# f.write(']')
-# f.close()
-
 import glob
 os.chdir("/Users/tanay/Desktop/ResearchWork/2023")
 count = 0
 for file in glob.glob("*.xml"):
-    # print(file)
     xml_file = file
-    # count += 1
     json_file = Path(xml_file).stem
     json_file = json_file + ".json"
     f = open(json_file, 'w+')
@@ -54,5 +36,3 @@ for file in glob.glob("*.xml"):
     f.write('}\n')
     f.write(']')
     f.close()
-
-# print(count)
